9b3567e923179068d8d9976091536a80ddda84e1
Andrew Lovseth <andrew.lovseth@me.com>
2021-05-25 15:36:26 -0700

    Divisions options page

--- functions/acf.php
+++ functions/acf.php
@@ -11,6 +11,7 @@ if(function_exists('acf_add_options_page')) {
     acf_add_options_sub_page('Header');
     acf_add_options_sub_page('Footer');
     acf_add_options_sub_page('Members');
+    acf_add_options_sub_page('Divisions');
 }
 
 
