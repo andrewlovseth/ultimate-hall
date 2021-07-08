b1f7bc80b7175dc4701d830a9aca5f458cc669bf
Andrew Lovseth <andrew.lovseth@me.com>
2021-07-08 10:25:50 -0700

    Updates (Donate, Vision)

--- templates/about/mission.php
+++ templates/about/mission.php
@@ -1,19 +1,34 @@
 <?php
 
     $mission = get_field('mission');
-    $headline = $mission['headline'];
-    $copy = $mission['copy'];
+    $mission_headline = $mission['headline'];
+    $mission_copy = $mission['copy'];
+
+
+    $vision = get_field('vision');
+    $vision_headline = $vision['headline'];
+    $vision_copy = $vision['copy'];
 
 ?>
 
-<aside class="sidebar mission">
-    <div class="info">
+<aside class="sidebar">
+    <div class="mission module">
         <div class="section-header">
-            <h3 class="small"><?php echo $headline; ?></h3>
+            <h3 class="small"><?php echo $mission_headline; ?></h3>
         </div>
 
         <div class="copy p3">
-            <?php echo $copy; ?>
+            <?php echo $mission_copy; ?>
+        </div>
+    </div>
+
+    <div class="vision module">
+        <div class="section-header">
+            <h3 class="small"><?php echo $vision_headline; ?></h3>
+        </div>
+
+        <div class="copy p3">
+            <?php echo $vision_copy; ?>
         </div>
     </div>
 </aside>
--- templates/contact/donate.php
+++ templates/contact/donate.php
@@ -3,7 +3,7 @@
     $donate = get_field('donate');
     $headline =  $donate['headline']; 
     $copy =  $donate['copy']; 
-    $link =  $donate['link']; 
+    $paypal_embed =  $donate['paypal_embed']; 
 
 ?>
 
@@ -13,23 +13,10 @@
             <h3 class="small"><?php echo $headline; ?></h3>
         </div>
 
-        <div class="copy p3">
+        <div class="copy p3 extended">
             <?php echo $copy; ?>
         </div>
 
-        <?php 
-            if( $link ): 
-            $link_url = $link['url'];
-            $link_title = $link['title'];
-            $link_target = $link['target'] ? $link['target'] : '_self';
-        ?>
-
-            <div class="cta">
-                <a class="btn" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-            </div>
-
-        <?php endif; ?>
+        <?php echo $paypal_embed; ?>
     </div>
-
-
 </section>
