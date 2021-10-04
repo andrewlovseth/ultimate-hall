597c71ab93e2abacd40c634eff24b29d9fecff9e
Andrew Lovseth <andrew.lovseth@me.com>
2021-10-04 08:36:02 -0700

    Post byline

--- single-post.php
+++ single-post.php
@@ -11,6 +11,32 @@
 				<div class="title">
 					<h1><?php the_title(); ?></h1>
 				</div>
+
+				<?php $authors = get_field('author'); if($authors): ?>
+					<div class="byline">
+						<?php foreach($authors as $author): ?>
+							<?php 
+								$image = get_field('photos_headshot', $author->ID); 
+							?>
+
+							<div class="author">
+								<div class="photo">
+									<a href="<?php echo get_permalink($author->ID); ?>">
+										<?php if($image): ?>
+											<img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+										<?php else: ?>
+											<div class="empty"></div>
+										<?php endif; ?>
+									</a>
+								</div>
+
+								<div class="info">
+									<a href="<?php echo get_permalink($author->ID); ?>"><?php echo get_the_title($author->ID); ?></a>
+								</div>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
 			</div>
 
 			<div class="article-body copy p1 extended">
