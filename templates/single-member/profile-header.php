b0ff6a6b46edac484a2f2d14b7910d5f402215ff
Andrew Lovseth <andrew.lovseth@me.com>
2023-01-26 09:23:09 -0800

    Induction type front-end updates

--- templates/single-member/profile-header.php
+++ templates/single-member/profile-header.php
@@ -7,24 +7,21 @@
 ?>
 
 <section class="profile-header">    
-        <div class="class">
-            <h2>
-                <?php if($year == '2004'): ?>Inaugural<?php endif; ?>
-                
-                Class of <?php echo $year; ?>
-                
-                    <?php if($induction_type): ?>
-                 - <?php echo $induction_type; ?>
-                <?php endif; ?>
-
-                <?php if($induction_division): ?>
-                 - <?php echo $induction_division['label']; ?>
-                <?php endif; ?>
-            </h2>
-        </div>
-
+    <div class="class">
+        <h2 class="class__title"><?php if($year == '2004'): ?>Inaugural <?php endif; ?> Class of <?php echo $year; ?></h2>
+    </div>
 
     <div class="name">
-        <h1><?php the_title(); ?></h1>
+        <h1 class="name__title"><?php the_title(); ?></h1>
+    </div>
+
+    <div class="meta">
+        <?php if($induction_type): ?>
+            <span class="meta__type"><?php echo $induction_type; ?></span>
+        <?php endif; ?>
+
+        <?php if($induction_division): ?>
+            <span class="meta__division"><?php echo $induction_division['label']; ?></span>
+        <?php endif; ?>        
     </div>
 </section>
