f5b2555a9145203ef8b64d579e20f70354c43ccc
Andrew Lovseth <andrew.lovseth@me.com>
2024-03-18 11:22:27 -0700

    Induction speech

--- templates/single-member/biography.php
+++ templates/single-member/biography.php
@@ -4,6 +4,8 @@
     $photos = get_field('photos');
     $player_card = $photos['player_card'];
 
+    $induction_speech = get_field('induction_speech');
+
 ?>
 
 <section class="biography">
@@ -35,6 +37,20 @@
                 <?php endwhile; ?>
             </ul>
         </section>
+    <?php endif; ?>
+
+    <?php if($induction_speech): ?>
+        <section class="induction-speech">
+            <div class="section-header">
+                <h3 class="small">Induction Speech</h3>
+            </div>
+
+            <div class="cta">
+                <a href="<?php echo $induction_speech['url']; ?>" class="btn btn__charcoal">Read <?php the_title(); ?>'s Induction Speech</a>
+            </div>
+
+        </section>
+
     <?php endif; ?>
     
 
